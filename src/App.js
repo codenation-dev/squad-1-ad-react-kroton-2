55b1b6287b2f08e541f80c58e0c0138f2d7cd48e
degrecci <henrique.degrecci@upx.com>
2020-01-20 19:48:11 -0300

    Add material-ui dependencies :heavy_plus_sign:

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +9,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="btn btn-primary"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="btn btn-primary"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ImportContactsIcon />
+            Learn React
+          </a>
+        </Card>
       </header>
     </div>
   );
