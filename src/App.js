24136a349048e9f45cd89b450729d3332422375a
degrecci <henrique.degrecci@upx.com>
2020-01-20 19:40:29 -0300

    Add bootstrap dependency :heavy_plus_sign:

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="btn btn-primary"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
