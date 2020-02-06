faf70163002192a4acb8a4013bfe58e468c72806
Alessandra <alessandraamontanher@gmail.com>
2020-02-05 21:23:42 -0300

    init forgotpassword js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import ImportContactsIcon from '@material-ui/icons/ImportContacts';
-
-import logo from './logo.svg';
+import ForgotPassword from './pages/ForgotPassword';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Card>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="btn btn-primary"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <ImportContactsIcon />
-            Learn React
-          </a>
-        </Card>
-      </header>
+      <ForgotPassword />
     </div>
   );
 }
---
+++ src/pages/ForgotPassword/index.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+export default function ForgotPassword() {
+  return <div>Index.js</div>;
+}
