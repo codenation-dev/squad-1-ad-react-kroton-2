bea06f9028bf4ab18804a8a47eebb77ae0a1cbce
Alessandra <alessandraamontanher@gmail.com>
2020-02-06 22:33:13 -0300

    CircularProgress

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { auth } from '../../firebase/config';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('');
+  const [firebaseReturn, setFirebaseReturn] = useState(false);
 
   const resetPassword = () => {
+    setFirebaseReturn(true);
     if (email) {
-      console.log(email);
     }
   };
   return (
     <div>
       <Typography variant="h4">Reset Password</Typography>
+      <br></br>
       <TextField
         required
         id="email"
@@ -27,6 +31,9 @@ export default function ForgotPassword() {
       <Button variant="contained" color="primary" onClick={resetPassword}>
         reset
       </Button>
+      <br></br>
+      <br></br>
+      {firebaseReturn && <CircularProgress />}
     </div>
   );
 }
