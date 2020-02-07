08e8f9755419756d1c348911bcc14454adb18bcb
Alessandra <alessandraamontanher@gmail.com>
2020-02-06 22:18:11 -0300

    colocando estilos de button, textfield e typography

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 export default function ForgotPassword() {
+  const [email, setEmail] = useState('');
+
+  const resetPassword = () => {
+    if (email) {
+      console.log(email);
+    }
+  };
   return (
     <div>
-      <p>Password Retrieve</p>
-      <label>E-mail</label>
-      <input placeholder="E-mail"></input>
+      <Typography variant="h4">Reset Password</Typography>
+      <TextField
+        required
+        id="email"
+        label="E-mail"
+        variant="outlined"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+      />
       <br></br>
-      <button>Retrieve</button>
+      <br></br>
+      <Button variant="contained" color="primary" onClick={resetPassword}>
+        reset
+      </Button>
     </div>
   );
 }
