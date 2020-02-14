6cefd62434014597a2d5fd2c9229a20f114d1076
delacquab <47065398+delacquab@users.noreply.github.com>
2020-02-14 11:39:45 -0300

    Cadastro

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
+import * as firebase from 'firebase/app';
 
-function Cadastro() {
-  return <h1>Cadastro</h1>;
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    width: '300px',
+  },
+}));
+
+const defaultProps = {
+  bgcolor: 'background.paper',
+  borderColor: 'primary.main',
+  mt: 10,
+  border: 1
+};
+
+export default function Cadastro({ history }) {
+  const classes = useStyles();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState(false);
+  const [erro, setErro] = useState('');
+
+  async function createAccount() {
+    try {
+      const usuario = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      alert("Usuário cadastro com sucesso.");
+      history.push('/Painel');
+    } catch (error) {
+      setErro(error);
+      setAlerta(true);
+    }
+  }
+
+  return (
+    <Container component="main" maxWidth="xs" >
+      <Box borderRadius={15} {...defaultProps}>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Cadastro
+                </Typography>
+
+          <TextField
+            required
+            variant="outlined"
+            margin="normal"
+            className="meuInput"
+            id="email"
+            type="email"
+            label="e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <TextField
+            required
+            variant="outlined"
+            margin="normal"
+            className="meuInput"
+            type="password"
+            id="password"
+            label="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+
+          {alerta &&
+            <Alert className={classes.submit} variant="filled" severity="error">{erro.message}</Alert>
+          }
+
+          <Button
+            className={classes.submit}
+            variant="contained"
+            color="primary"
+            onClick={createAccount}>
+            Cadastrar
+                </Button>
+        </div>
+      </Box>
+    </Container>
+  )
 }
-
-export default Cadastro;
