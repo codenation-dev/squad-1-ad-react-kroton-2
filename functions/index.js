cfa32ad900c9ff8e675ffff70c9d841a28e8455b
degrecci <henrique.degrecci@upx.com>
2020-02-29 12:03:16 -0300

    Add data by user id in url

--- functions/index.js
+++ functions/index.js
@@ -11,22 +11,27 @@ const getRandomValueFrom = array => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
+const payload = {
+  detalhes: 'bla bla bla',
+  descricao: 'Ipsum dolor...',
+  titulo: 'Erro 404',
+  ambiente: getRandomValueFrom(ambientes),
+  origem: getRandomValueFrom(origens),
+  coletadoPor: 'usuário XPTO',
+  eventos: 2,
+  level: getRandomValueFrom(leveis),
+  criadoEm: new Date()
+};
+
 exports.createData = functions.https.onRequest((request, response) => {
+  const userId = request.url.substr(1);
+
   const usersCollectionRef = admin.firestore().collection('usuários');
-  const userDocumentRef = usersCollectionRef.doc(
-    'zncYjnoSGKUpAlqtCShzaPlg20Z2'
-  );
+  const userDocumentRef = usersCollectionRef.doc(userId);
   const alertCollectionRef = userDocumentRef.collection('alertas');
 
-  return alertCollectionRef.add({
-    detalhes: 'bla bla bla',
-    descricao: 'Ipsum dolor...',
-    titulo: 'Erro 404',
-    ambiente: getRandomValueFrom(ambientes),
-    origem: getRandomValueFrom(origens),
-    coletadoPor: 'usuário XPTO',
-    eventos: 2,
-    level: getRandomValueFrom(leveis),
-    criadoEm: new Date()
-  });
+  response.send(
+    `Os dados ${JSON.stringify(payload)} serão inseridos no usuário: ${userId}`
+  );
+  return alertCollectionRef.add({ ...payload });
 });
