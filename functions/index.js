4da2abdc5de6e51bf407229f30fd7de0d1520e95
degrecci <henrique.degrecci@upx.com>
2020-02-29 11:47:16 -0300

    Add insertion with randomic items

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const ambientes = ['Produção', 'Homologação', 'Desenvolvimento'];
+const leveis = ['error', 'warning', 'debug'];
+const origens = ['127.0.0.1', '10.0.1.1', 'app.server.com.br'];
+
+const getRandomValueFrom = array => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+exports.createData = functions.https.onRequest((request, response) => {
+  const usersCollectionRef = admin.firestore().collection('usuários');
+  const userDocumentRef = usersCollectionRef.doc(
+    'zncYjnoSGKUpAlqtCShzaPlg20Z2'
+  );
+  const alertCollectionRef = userDocumentRef.collection('alertas');
+
+  return alertCollectionRef.add({
+    detalhes: 'bla bla bla',
+    descricao: 'Ipsum dolor...',
+    titulo: 'Erro 404',
+    ambiente: getRandomValueFrom(ambientes),
+    origem: getRandomValueFrom(origens),
+    coletadoPor: 'usuário XPTO',
+    eventos: 2,
+    level: getRandomValueFrom(leveis),
+    criadoEm: new Date()
+  });
+});
