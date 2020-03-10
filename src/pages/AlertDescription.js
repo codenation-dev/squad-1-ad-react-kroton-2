def3a5a8c8bb02f7ab96b7f589383c713f03ddc5
alessandra <alessandraamontanher@gmail.com>
2020-03-10 12:19:51 -0300

    chamando docs do firebase

--- src/pages/AlertDescription.js
+++ src/pages/AlertDescription.js
@@ -13,31 +13,21 @@ import AlertaNav from '../components/Alert/AlertaNav';
 import BarraUm from '../components/BarraUm';
 import * as firebase from 'firebase/app';
 
-const getAlertById = async id => {
-  const response = await fetch(``);
-
-  // tratamento do retorno
-  // se não existir > 404
-  // se exist > response.json()
-  return {
-    detalhes: 'Sorry, that page cannot be found. Page: /produtos/458796',
-    descricao: 'Ipsum dolor...',
-    titulo: 'Erro 404',
-    ambiente: 'Produção',
-    origem: '10.0.1.1',
-    coletadoPor: 'usuário XPTO',
-    eventos: 2728,
-    level: 'debug',
-    criadoEm: '2020-03-05T14:08:57.725Z',
-    arquivado: false
-  };
-};
-
 const useStyles = makeStyles(theme => ({
   container: {
     marginTop: 20
   }
 }));
+
+const getAlertById = async (uid, id) => {
+  const db = firebase.firestore();
+  return db
+    .collection('usuários')
+    .doc(uid)
+    .collection('alertas')
+    .doc(id)
+    .get();
+};
 
 function AlertDescription(props) {
   const classes = useStyles();
@@ -50,15 +40,11 @@ function AlertDescription(props) {
 
   React.useEffect(() => {
     const { uid } = firebase.auth().currentUser;
+    const id = props.match.params.id;
 
-    console.log(uid);
-
-    const alertId = props.match.params.id;
-    getAlertById(alertId).then(r => {
-      setAlert(r);
-      setTimeout(() => {
-        setShowAlert(true);
-      }, 500);
+    getAlertById(uid, id).then(r => {
+      setAlert(r.data());
+      setShowAlert(true);
     });
   }, []);
 
